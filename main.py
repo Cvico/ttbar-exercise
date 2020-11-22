ecea5e013f403d1a4254cd9b3d39f3a9e4a802c6
Carlos <cvicovillalba1998@gmail.com>
2020-11-22 15:31:38 +0100

    small changes

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import numpy as np
 from copy import deepcopy
 import Functions as F
 import ROOT as r
+from test import FAEA_Analysis
 import argparse
 import pandas as pd
+
+
+def add_parsing_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--Path', '-P', metavar="path", dest="path", default="../", help = "Path where the ntuples are stored")
+    return parser
+
+
+if __name__ == "__main__":
+    # Create a parser where to specify actions
+    parser = add_Parsing_options()
+    an = FAEA_Analysis('./plots.txt', 'data')
+
 
